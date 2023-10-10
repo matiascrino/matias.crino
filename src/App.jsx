520099492c357295f253f316c66ec048c9e6619e
Matias Crino <matiascrino@gmail.com>
2023-10-10 16:21:04 -0300

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AboutMeSection from "./components/MainSection";
 import HeroSection from "./components/HeroSection";
 import LandingHeader from "./components/LandingHeader";
+import CursorShadow from "./components/CursorShadow.jsx";
 
 function App() {
 	return (
 		<>
+			<CursorShadow />
 			<LandingHeader />
 			<main class="snap-y snap-mandatory relative w-full h-screen overflow-auto bg-background-color font-acumin">
 				<div class="snap-start">
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "../dist/output.css";
 import App from './App.jsx'
-import CursorShadow from './components/CursorShadow.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
-      <CursorShadow />
       <App />
     </LanguageProvider>,
   </React.StrictMode>
